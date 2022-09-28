24dd783952d463e5ba61b8da1c436e08addab942
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-09-28 13:03:07 +0000

    You are not allowed to google anything

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], number=0):
-    return(list(map((number).__mul__, my_list)))
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return (0)
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    integer = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
+            integer += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
+        else:
+            integer += roman[roman_string[i]]
+    return (integer)
